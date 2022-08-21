eab7a8fdd2a325726420ff59ce24de1818c8091d
Mohammed AD <moad90300@gmail.com>
2022-08-21 13:26:51 +0300

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,275 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/37f0e86244.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/medium.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Services</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Location</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Content</a>
+                    </li>
+
+
+                </ul>
+                <form class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+    <!-- **********************************  END NAVBAR ************************************* -->
+    <!-- **********************************  END home ************************************* -->
+    <header class="home">
+        <div class="container">
+            <div class="overlay"></div>
+            <div class="home-p">
+                <h2>Faucibus Porttitor Metus</h2>
+                <p>In nibh nullam egestas velit laoreet nullam elementum ipsum pharetra suscipit leo augue pretium felis
+                    nisl vitae ipsum curabitur quis libero.</p>
+                <button type="button" class="btn btn-primary">Primary</button>
+
+            </div>
+        </div>
+    </header>
+
+
+    <!-- *****************************************end home  **************************** -->
+
+
+    <section class="about">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-3 col-md-3 col-sm-12">
+                    <div class="item1 item">
+                        <i class="fas fa-hourglass-half"></i>
+                        <h6>Feugiat Fermentum</h6>
+                        <p>Ac orci proin porttitor lacus eget mi pellentesque non.</p>
+
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-12">
+                    <div class="item2 item">
+                        <i class="fas fa-hourglass-half"></i>
+                        <h6>Feugiat Fermentum</h6>
+                        <p>Ac orci proin porttitor lacus eget mi pellentesque non.</p>
+
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-12">
+                    <div class="item3 item">
+                        <i class="fas fa-hourglass-half"></i>
+                        <h6>Feugiat Fermentum</h6>
+                        <p>Ac orci proin porttitor lacus eget mi pellentesque non.</p>
+
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-12">
+                    <div class="item4 item">
+                        <i class="fas fa-hourglass-half"></i>
+                        <h6>Feugiat Fermentum</h6>
+                        <p>Ac orci proin porttitor lacus eget mi pellentesque non.</p>
+
+                    </div>
+                </div>
+
+                <div class="col-lg-6 col-md-6 col-sm-12">
+                    <div class="col">
+                        <div class="card">
+                            <img src="img/01.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a longer card with supporting text below as a natural
+                                    lead-in to additional content. This content is a little bit longer.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-12">
+                    <div class="col">
+                        <div class="card">
+                            <img src="img/02.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a longer card with supporting text below as a natural
+                                    lead-in to additional content. This content is a little bit longer.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+    </section>
+
+    <hr>
+    <!-- ******************************* End About ************************* -->
+    <!-- ******************************* Start service ************************* -->
+
+
+    <section class="services" id="Services">
+        <div class="container">
+            <h2>My Services</h2>
+            <div class="row">
+                <div class="col-lg-4 col-md-4">
+                    <div class="card text-center">
+                        <div class="card-body">
+                            <h5 class="card-title">Web Design </h5>
+                            <div class="line"></div>
+                            <p class="card-text">I have the ability and work to design an integrated website that is
+                                responsive to all screens using the required technologies</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4">
+                    <div class="card text-center">
+                        <div class="card-body">
+                            <h5 class="card-title">Developer </h5>
+                            <div class="line"></div>
+                            <p class="card-text">I can work on your project using one of the JavaScript library React Js
+                                and its framework, The project will fast, light and smooth with high quality.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4">
+                    <div class="card text-center">
+                        <div class="card-body">
+                            <h5 class="card-title">Designer</h5>
+                            <div class="line"></div>
+                            <p class="card-text">I can work on your project using one of the JavaScript library React Js
+                                and its framework, The project will fast, light and smooth with high quality.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <hr>
+
+    <!-- ******************************* End service ************************* -->
+    <!-- ******************************* Start skills ************************* -->
+
+
+
+    <div class="map" id="contact">
+        <h2>Locations</h2>
+        <iframe
+            src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Twitter,+Inc.,+Market+Street,+San+Francisco,+CA&amp;aq=0&amp;oq=twitter&amp;sll=28.659344,-81.187888&amp;sspn=0.128789,0.264187&amp;ie=UTF8&amp;hq=Twitter,+Inc.,+Market+Street,+San+Francisco,+CA&amp;t=m&amp;z=15&amp;iwloc=A&amp;output=embed"></iframe>
+        <br />
+        <small><a
+                href="https://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=Twitter,+Inc.,+Market+Street,+San+Francisco,+CA&amp;aq=0&amp;oq=twitter&amp;sll=28.659344,-81.187888&amp;sspn=0.128789,0.264187&amp;ie=UTF8&amp;hq=Twitter,+Inc.,+Market+Street,+San+Francisco,+CA&amp;t=m&amp;z=15&amp;iwloc=A"></a></small>
+    </div>
+
+
+
+    <!-- ******************************* End skills ************************* -->
+    <!-- ******************************* start content ************************* -->
+    <hr>
+    <section class="content">
+        <div class="container">
+            <h2>Content Us</h2>
+            <div class="row">
+                <div class="input-group mb-3">
+                    <span class="input-group-text" id="basic-addon1">@</span>
+                    <input type="text" class="form-control" placeholder="Username" aria-label="Username"
+                        aria-describedby="basic-addon1">
+                </div>
+
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="Recipient's username"
+                        aria-label="Recipient's username" aria-describedby="basic-addon2">
+                    <span class="input-group-text" id="basic-addon2">@example.com</span>
+                </div>
+
+                <label for="basic-url" class="form-label">Your vanity URL</label>
+                <div class="input-group mb-3">
+                    <span class="input-group-text" id="basic-addon3">https://example.com/users/</span>
+                    <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3">
+                </div>
+
+                <div class="input-group mb-3">
+                    <span class="input-group-text">$</span>
+                    <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)">
+                    <span class="input-group-text">.00</span>
+                </div>
+
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="Username" aria-label="Username">
+                    <span class="input-group-text">@</span>
+                    <input type="text" class="form-control" placeholder="Server" aria-label="Server">
+                </div>
+
+                <div class="input-group">
+                    <span class="input-group-text">With textarea</span>
+                    <textarea class="form-control" aria-label="With textarea"></textarea>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+    <!-- ******************************* End content ************************* -->
+
+    <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-md-6">
+                    <span class="span"> @2021 All Rights Reserved</span>
+                </div>
+                <div class="icon col-lg-6 col-md-6">
+                    <a href="" target="_black"><i class="fab fa-facebook"></i></a>
+                    <a href="" target="_black"><i class="fab fa-twitter-square"></i></a>
+                    <a href="" target="_black"><i class="fab fa-invision"></i></a>
+                    <a href="" target="_black"><i class="fab fa-instagram-square"></i></a>
+                    <a href="" target="_black"><i class="fas fa-envelope"></i></a>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+
+
+
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+        integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
